ed4e8d8302cfe14aa3002e76ccd0afb4105a2771
tassadit1996 <adoudou.mgh@gmail.com>
2024-01-10 21:29:44 -0300

    205

--- src/components/form/AuthInputField.tsx
+++ src/components/form/AuthInputField.tsx
@@ -1,8 +1,9 @@
 import AppInput from '@ui/AppInput';
 import colors from '@utils/colors';
 import { useFormikContext } from 'formik';
-import {FC} from 'react';
+import {FC, useEffect} from 'react';
 import {View, StyleSheet, Text, TextInput, TextInputProps, StyleProp, ViewStyle} from 'react-native';
+import Animated, { useAnimatedStyle, useSharedValue, withSequence, withSpring, withTiming} from 'react-native-reanimated';
 
 interface Props {
   name: string;
@@ -18,6 +19,8 @@ interface Props {
 }
 
 const AuthInputField: FC<Props> = props => {
+  const inputTransformValue = useSharedValue(0)
+
   const {handleChange, values, errors, touched, handleBlur} = useFormikContext<{[key: string]: string}>()
 
   const {
@@ -31,12 +34,33 @@ const AuthInputField: FC<Props> = props => {
 } = props;
 
   const errorMsg = touched[name] && errors[name]? errors[name] : '';
+
+  const shakeUI = () => {
+    inputTransformValue.value = withSequence(
+      withTiming(-10, {duration: 50}),
+      withSpring(0, {
+        damping: 8,
+        mass: 0.5,
+        stiffness: 1000,
+        restDisplacementThreshold: 0.1
+      })
+    )
+  }
   
+  const inputStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{translateX: inputTransformValue.value}]
+    }
+  })
+
+  useEffect(() => {
+    if(errorMsg) shakeUI()
+  }, [errorMsg])
 
 
 
   return (
-    <View style={[styles.container, containerStyle]}>
+    <Animated.View style={[containerStyle, inputStyle]}>
       <View style={styles.labelContainer}>
         <Text style={styles.label}>{label}</Text>
         <Text style={styles.errorMsg}>{errorMsg}</Text>
@@ -51,14 +75,12 @@ const AuthInputField: FC<Props> = props => {
         value={values[name]}
         onBlur={handleBlur(name)}
          />
-    </View>
+    </Animated.View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
 
-  },
   label: {
     color: colors.CONTRAST,
   },
@@ -69,7 +91,8 @@ const styles = StyleSheet.create({
     flexDirection: 'row', 
     alignItems: 'center',
     justifyContent: 'space-between',
-    padding: 5
+    padding: 5,
+
   },
 
 });
