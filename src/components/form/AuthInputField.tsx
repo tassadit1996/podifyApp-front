c7aaeef855c6bcebfbf6b4c8078c5e085c330d26
tassadit1996 <adoudou.mgh@gmail.com>
2024-01-12 14:42:46 -0300

    208 - fixing erro

--- src/components/form/AuthInputField.tsx
+++ src/components/form/AuthInputField.tsx
@@ -1,9 +1,24 @@
 import AppInput from '@ui/AppInput';
 import colors from '@utils/colors';
-import { useFormikContext } from 'formik';
+import {useFormikContext} from 'formik';
 import {FC, ReactNode, useEffect} from 'react';
-import {View, StyleSheet, Text, TextInput, TextInputProps, StyleProp, ViewStyle, Pressable} from 'react-native'; 
-import Animated, { useAnimatedStyle, useSharedValue, withSequence, withSpring, withTiming} from 'react-native-reanimated';
+import {
+  View,
+  StyleSheet,
+  Text,
+  TextInput,
+  TextInputProps,
+  StyleProp,
+  ViewStyle,
+  Pressable,
+} from 'react-native';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSequence,
+  withSpring,
+  withTiming,
+} from 'react-native-reanimated';
 
 interface Props {
   name: string;
@@ -12,18 +27,19 @@ interface Props {
   errorMsg?: string;
   placeholder?: string;
   keyboardType?: TextInputProps['keyboardType'];
-  autoCapitalize?: TextInputProps['autoCapitalize']
-  secureTextEntry?:boolean;
+  autoCapitalize?: TextInputProps['autoCapitalize'];
+  secureTextEntry?: boolean;
   containerStyle?: StyleProp<ViewStyle>;
   rightIcon?: ReactNode;
-  onRightIconPress?(): void
- 
+  onRightIconPress?(): void;
 }
 
 const AuthInputField: FC<Props> = props => {
-  const inputTransformValue = useSharedValue(0)
+  const inputTransformValue = useSharedValue(0);
 
-  const {handleChange, values, errors, touched, handleBlur} = useFormikContext<{[key: string]: string}>()
+  const {handleChange, values, errors, touched, handleBlur} = useFormikContext<{
+    [key: string]: string;
+  }>();
 
   const {
     label,
@@ -34,11 +50,10 @@ const AuthInputField: FC<Props> = props => {
     containerStyle,
     name,
     rightIcon,
-    onRightIconPress
+    onRightIconPress,
+  } = props;
 
-} = props;
-
-  const errorMsg = touched[name] && errors[name]? errors[name] : '';
+  const errorMsg = touched[name] && errors[name] ? errors[name] : '';
 
   const shakeUI = () => {
     inputTransformValue.value = withSequence(
@@ -47,75 +62,69 @@ const AuthInputField: FC<Props> = props => {
         damping: 8,
         mass: 0.5,
         stiffness: 1000,
-        restDisplacementThreshold: 0.1
-      })
-    )
-  }
-  
+        restDisplacementThreshold: 0.1,
+      }),
+    );
+  };
+
   const inputStyle = useAnimatedStyle(() => {
     return {
-      transform: [{translateX: inputTransformValue.value}]
-    }
-  })
+      transform: [{translateX: inputTransformValue.value}],
+    };
+  });
 
   useEffect(() => {
-    if(errorMsg) shakeUI()
-  }, [errorMsg])
-
-
+    if (errorMsg) shakeUI();
+  }, [errorMsg]);
 
   return (
     <Animated.View style={[containerStyle, inputStyle]}>
       <View style={styles.labelContainer}>
         <Text style={styles.label}>{label}</Text>
         <Text style={styles.errorMsg}>{errorMsg}</Text>
-
       </View>
       <View>
-
+        <AppInput
+          placeholder={placeholder}
+          keyboardType={keyboardType}
+          autoCapitalize={autoCapitalize}
+          secureTextEntry={secureTextEntry}
+          onChangeText={handleChange(name)}
+          value={values[name]}
+          onBlur={handleBlur(name)}
+        />
+        {rightIcon ? (
+          <Pressable onPress={onRightIconPress} style={styles.rightIcon}>
+            {rightIcon}
+          </Pressable>
+        ) : null}
       </View>
-      <AppInput 
-        placeholder={placeholder}
-        keyboardType={keyboardType} 
-        autoCapitalize={autoCapitalize}
-        secureTextEntry={secureTextEntry}
-        onChangeText={handleChange(name)}
-        value={values[name]}
-        onBlur={handleBlur(name)}
-         />
-      {rightIcon ? <Pressable onPress={onRightIconPress} style={styles.rightIcon}>
-        {rightIcon}
-      </Pressable>: null}
     </Animated.View>
   );
 };
 
 const styles = StyleSheet.create({
-
   label: {
     color: colors.CONTRAST,
   },
   errorMsg: {
-    color: colors.ERROR
+    color: colors.ERROR,
   },
   labelContainer: {
-    flexDirection: 'row', 
+    flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
     padding: 5,
-
   },
   rightIcon: {
     width: 45,
     height: 45,
     position: 'absolute',
-    top: 0,
     right: 0,
+    top: 0,
     justifyContent: 'center',
     alignItems: 'center',
-
-  }
-
+  },
 });
 
 export default AuthInputField;
