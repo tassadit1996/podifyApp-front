aa62bc31950e8b23ad0ca94c1b1af22c2d2cb878
tassadit1996 <adoudou.mgh@gmail.com>
2024-01-15 16:39:53 -0300

    216

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 import LostPassword from "@views/auth/LostPassword";
 import SignIn from "@views/auth/SignIn";
+import Verification from "@views/auth/Verification";
 
 
 const App = () => {
-  return <LostPassword/>
+  return <Verification/>
 };
 
 export default App;
