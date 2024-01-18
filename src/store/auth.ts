f924b748f8c8805c6a4a571cdabc97f7678a5573
tassadit1996 <adoudou.mgh@gmail.com>
2024-01-18 17:12:56 -0300

    243

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -1,13 +1,46 @@
-import { createSlice } from "@reduxjs/toolkit";
+import {createSlice, PayloadAction, createSelector} from '@reduxjs/toolkit';
+import { getReduceMotionFromConfig } from 'react-native-reanimated/lib/typescript/reanimated2/animation/util';
+import { RootState } from '.';
 
+export interface UserProfile {
+  id: string;
+  name: string;
+  email: string;
+  verified: boolean;
+  avatar?: string;
+  followers: number;
+  followings: number;
+}
+interface AuthState {
+  profile: UserProfile | null;
+  loggedIn: boolean;
+}
 
-const initialState = {}
-
+const initialState: AuthState = {
+  profile: null,
+  loggedIn: false,
+};
 
 const slice = createSlice({
-    name: "auth",
-    initialState,
-    reducers: {}
-})
+  name: 'auth',
+  initialState,
+  reducers: {
+    updateProfile(authState, {payload}: PayloadAction<UserProfile | null>) {
+      authState.profile = payload;
+    },
+    updateLoggedInState(authState, {payload}) {
+      authState.loggedIn = payload;
+    },
+  },
+});
 
-export default slice.reducer
+export const {updateLoggedInState, updateProfile} = slice.actions;
+
+export const getAuthState = createSelector(
+    (state: RootState) => state,
+     authState => authState,
+     
+
+)
+
+export default slice.reducer;
--- src/store/index.ts
+++ src/store/index.ts
@@ -7,5 +7,7 @@ const store = configureStore({
     reducer: authReducer
 })
 
+export type RootState = ReturnType<typeof store.getState>
+
 export default store;
 
