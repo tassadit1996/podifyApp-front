fa72cb9ea44b53026230e97e676756bbbd07e704
tassadit1996 <adoudou.mgh@gmail.com>
2024-01-22 08:55:22 -0300

    246

--- App.tsx
+++ App.tsx
@@ -1,14 +1,11 @@
-import {NavigationContainer} from '@react-navigation/native';
 import {Provider} from 'react-redux';
-import AuthNavigator from 'src/navigation/AuthNavigation';
+import AppNavigator from 'src/navigation';
 import store from 'src/store';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <AuthNavigator />
-      </NavigationContainer>
+      <AppNavigator />
     </Provider>
   );
 };
