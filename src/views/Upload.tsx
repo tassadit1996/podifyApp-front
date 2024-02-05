ca207d14b75600d477026168d0fd21e969b56683
tassadit1996 <adoudou.mgh@gmail.com>
2024-02-05 14:17:31 -0300

    252

--- src/views/Upload.tsx
+++ src/views/Upload.tsx
@@ -1,15 +1,15 @@
-
 import FileSelector from '@components/FileSelector';
 import colors from '@utils/colors';
 import {FC} from 'react';
-import {View, StyleSheet, Text, Pressable} from 'react-native';
+import {View, StyleSheet, Text, Pressable, TextInput, ScrollView} from 'react-native';
+
 import MaterialComIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 interface Props {}
 
 const Upload: FC<Props> = props => {
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <View style={styles.fileSelctorContainer}>
         <FileSelector
           icon={
@@ -33,7 +33,19 @@ const Upload: FC<Props> = props => {
           style={{marginLeft: 20}}
         />
       </View>
-    </View>
+      <View style={styles.formContainer}>
+        <TextInput
+        placeholderTextColor={colors.INACTIVE_CONTRAST} 
+        placeholder="Title"
+        style={styles.input} />
+        <TextInput
+        placeholderTextColor={colors.INACTIVE_CONTRAST} 
+        placeholder="About"
+        style={styles.input} 
+        numberOfLines={10}
+        multiline/>
+      </View>
+    </ScrollView>
   );
 };
 
@@ -43,6 +55,19 @@ const styles = StyleSheet.create({
   },
   fileSelctorContainer: {
     flexDirection: 'row',
+  },
+  formContainer: {
+    marginTop: 20,
+  },
+  input: {
+    borderWidth: 2,
+    borderColor: colors.SECONDARY,
+    borderRadius: 7,
+    padding: 10,
+    fontSize: 18,
+    color: colors.CONTRAST,
+    marginBottom: 20,
+    textAlignVertical: 'top'
   },
 });
 
