8fb86508eea48db801009f3aa7d2f8a08799efa3
tassadit1996 <adoudou.mgh@gmail.com>
2024-02-06 16:37:32 -0300

    265

---
+++ src/components/form/AppNotification.tsx
@@ -0,0 +1,86 @@
+import colors from '@utils/colors';
+import {FC, useEffect} from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
+import {useDispatch, useSelector} from 'react-redux';
+import {
+  getNotificationState,
+  upldateNotification,
+} from 'src/store/notification';
+
+interface Props {}
+
+const AppNotification: FC<Props> = props => {
+  const {message, type} = useSelector(getNotificationState);
+  const height = useSharedValue(0);
+
+  const dispatch = useDispatch();
+
+  const heightStyle = useAnimatedStyle(() => {
+    return {
+      height: height.value,
+    };
+  });
+
+  let backgroundColor = colors.ERROR;
+  let textColor = colors.CONTRAST;
+
+  switch (type) {
+    case 'success':
+      backgroundColor = colors.SUCCESS;
+      textColor = colors.PRIMARY;
+      break;
+  }
+
+  useEffect(() => {
+    // Usa uma asserção de tipo para tratar o retorno de setTimeout como number.
+    let timeoutId: number;
+  
+    const performAnimation = () => {
+      height.value = withTiming(45, {
+        duration: 150,
+      });
+  
+      timeoutId = setTimeout(() => {
+        height.value = withTiming(0, {
+          duration: 150,
+        });
+  
+        dispatch(upldateNotification({ message: '', type: 'error' }));
+      }, 3000) as unknown as number; // Assegura que o retorno de setTimeout seja tratado como number.
+    };
+  
+    performAnimation();
+  
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [message]);
+  
+ 
+  
+
+  return (
+    <Animated.View style={[styles.container, {backgroundColor}, heightStyle]}>
+      <Text style={[styles.message, {color: textColor}]}>{message}</Text>
+    </Animated.View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  message: {
+    fontSize: 18,
+    alignItems: 'center',
+  },
+});
+
+export default AppNotification;
