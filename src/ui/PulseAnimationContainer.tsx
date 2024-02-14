e05742bf24fc9e687f05b76896451929b35c5f0b
tassadit1996 <adoudou.mgh@gmail.com>
2024-02-13 21:40:18 -0300

    271

--- src/ui/PulseAnimationContainer.tsx
+++ src/ui/PulseAnimationContainer.tsx
@@ -1,14 +1,19 @@
 import PulseAnimationContainer from '@ui/PulseAnimationContainer';
 import {FC} from 'react';
 import {View, StyleSheet, Text} from 'react-native';
+import {useQuery} from 'react-query';
+import {useDispatch} from 'react-redux';
+import catchAsyncError from 'src/api/catchError';
+import client from 'src/api/client';
 import {useFetchLatestAudios} from 'src/hooks/query';
+import {updateNotification} from 'src/store/notification';
 
 interface Props {}
 
 const Home: FC<Props> = props => {
   const {data, isLoading} = useFetchLatestAudios();
-  //if(isLoading)
 
+  // if (isLoading)
   return (
     <PulseAnimationContainer>
       <Text style={{color: 'white', fontSize: 25}}>Loading</Text>
@@ -18,7 +23,11 @@ const Home: FC<Props> = props => {
   return (
     <View style={styles.container}>
       {data?.map(item => {
-        return <Text key={item.id} style={{color: 'white', paddingVertical: 10}}>{item.title}</Text>
+        return (
+          <Text key={item.id} style={{color: 'white', paddingVertical: 10}}>
+            {item.title}
+          </Text>
+        );
       })}
     </View>
   );
