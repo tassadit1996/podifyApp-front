c74adae9d6789a90872d8fd8d051b85a3a4dea2f
tassadit1996 <adoudou.mgh@gmail.com>
2024-02-15 14:10:20 -0300

    281

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,11 +1,16 @@
 import LatestUploads from '@components/LatestUploads';
+import OptionsModal from '@components/OptionsModal';
 import RecommendedAudios from '@components/RecommendedAudios';
-import {FC} from 'react';
-import {View, StyleSheet} from 'react-native';
+import colors from '@utils/colors';
+import {FC, useState} from 'react';
+import {View, StyleSheet, Pressable, Text} from 'react-native';
+import MaterialComIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 interface Props {}
 
 const Home: FC<Props> = props => {
+  const [showOptions, setShowOptions] = useState(false);
+
   return (
     <View style={styles.container}>
       <LatestUploads
@@ -13,15 +18,39 @@ const Home: FC<Props> = props => {
           console.log(item);
         }}
         onAudioLongPress={() => {
-          console.log('long press');
+          setShowOptions(true);
         }}
       />
-      <RecommendedAudios onAudioPress={item => {
+      <RecommendedAudios
+        onAudioPress={item => {
           console.log(item);
         }}
         onAudioLongPress={() => {
-          console.log('long press');
-        }} />
+          setShowOptions(true);
+        }}
+      />
+      <OptionsModal
+        visible={showOptions}
+        onRequestClose={() => {
+          setShowOptions(false);
+        }}
+        options={[
+          {title: 'Add to playlist', icon: 'playlist-music'},
+          {title: 'Add to favorite', icon: 'cards-heart'},
+        ]}
+        renderItem={item => {
+          return (
+            <Pressable style={styles.optionContainer}>
+              <MaterialComIcon
+                size={24}
+                color={colors.PRIMARY}
+                name={item.icon}
+              />
+              <Text style={styles.optionLabel}>{item.title}</Text>
+            </Pressable>
+          );
+        }}
+      />
     </View>
   );
 };
@@ -30,6 +59,15 @@ const styles = StyleSheet.create({
   container: {
     padding: 10,
   },
+  optionContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 10,
+  },
+  optionLabel: {
+    color: colors.PRIMARY,
+    fontSize: 16,
+    marginLeft: 5},
 });
 
 export default Home;
