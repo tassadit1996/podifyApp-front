b3df3a47a687b0776859586237a19e108cea1af2
tassadit1996 <adoudou.mgh@gmail.com>
2024-02-15 09:16:24 -0300

    277

--- src/components/RecommendedAudios.tsx
+++ src/components/RecommendedAudios.tsx
@@ -1,4 +1,5 @@
 import GridView from '@ui/GridView';
+import PulseAnimationContainer from '@ui/PulseAnimationContainer';
 import colors from '@utils/colors';
 import {FC} from 'react';
 import {View, StyleSheet, Text, Image, Pressable} from 'react-native';
@@ -6,12 +7,28 @@ import {useFetchRecommendedtAudios} from 'src/hooks/query';
 
 interface Props {}
 
+const dummyData = new Array(6).fill('');
 const RecommendedAudios: FC<Props> = props => {
   const {data, isLoading} = useFetchRecommendedtAudios();
- 
-  const getPoster = (poster ?: string) => {
+
+  const getPoster = (poster?: string) => {
     return poster ? {uri: poster} : require('../assets/music.png');
   };
+  if (isLoading)
+  return (
+    <PulseAnimationContainer>
+      <View style={styles.container}>
+        <View style={styles.dummyTitleView} />
+        <GridView
+          col={3}
+          data={data || []}
+          renderItem={item => {
+            return <View style={styles.dummyAudioView} />;
+          }}
+        />
+      </View>
+    </PulseAnimationContainer>
+  );
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Latest Uploads</Text>
@@ -63,6 +80,19 @@ const styles = StyleSheet.create({
     marginTop: 5,
   },
   poster: {width: '100%', aspectRatio: 1, borderRadius: 7},
+  dummyTitleView: {
+    height: 20,
+    width: 150,
+    backgroundColor: colors.INACTIVE_CONTRAST,
+    marginBottom: 15,
+    borderRadius: 5,
+  },
+  dummyAudioView: {
+    width: '100%',
+    aspectRatio: 1,
+    backgroundColor: colors.INACTIVE_CONTRAST,
+    borderRadius: 5,
+  }
 });
 
 export default RecommendedAudios;
