9983b916620a60e193350ae28cf5b9f7995a6a90
tassadit1996 <adoudou.mgh@gmail.com>
2024-02-19 16:20:51 -0300

    294

--- src/components/profile/UploadsTab.tsx
+++ src/components/profile/UploadsTab.tsx
@@ -1,3 +1,4 @@
+import AudioListle from '@ui/AudioListle';
 import colors from '@utils/colors';
 import {FC} from 'react';
 import {
@@ -15,25 +16,10 @@ interface Props {}
 const UploadsTab: FC<Props> = props => {
   const {data, isLoading} = useFetchUploadsByProfile();
 
-  const getSource = (poster?: string) => {
-    return poster ? {uri: poster} : require('../../assets/music_small.png');
-  };
-
   return (
     <ScrollView style={styles.container}>
       {data?.map(item => {
-        return (
-          <Pressable style={styles.listItem} key={item.id}>
-            <Image source={getSource(item.poster)} style={styles.poster} />
-            <View style={styles.titleContainer}>
-              <Text style={styles.title} numberOfLines={1} ellipsizeMode="tail">
-                {item.title}
-              </Text>
-              <Text style={styles.owner} numberOfLines={1} ellipsizeMode="tail">
-                {item.owner.name}
-              </Text>
-            </View>
-          </Pressable>
+        return ( <AudioListle audio={item}/>
         );
       })}
     </ScrollView>
@@ -42,28 +28,7 @@ const UploadsTab: FC<Props> = props => {
 
 const styles = StyleSheet.create({
   container: {},
-  listItem: {
-    flexDirection: 'row',
-    backgroundColor: colors.OVERLAY,
-    marginBottom: 15,
-    borderRadius: 5,
-  },
-  titleContainer: {
-    flex: 1,
-    padding: 5,
-  },
-  poster: {
-    width: 50,
-    height: 50,
-  },
-  title: {
-    color: colors.CONTRAST,
-    fontWeight: '700',
-  },
-  owner: {
-    color: colors.SECONDARY,
-    fontWeight: '700',
-  },
+ 
 });
 
 export default UploadsTab;
