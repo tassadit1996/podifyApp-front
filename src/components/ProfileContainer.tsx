90183689387053dd1298f47e37a2ae476eb2663c
tassadit1996 <adoudou.mgh@gmail.com>
2024-02-20 09:02:57 -0300

    300

--- src/components/ProfileContainer.tsx
+++ src/components/ProfileContainer.tsx
@@ -1,8 +1,9 @@
 import {FC} from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, Text} from 'react-native';
 import { UserProfile } from 'src/store/auth';
 import AvatarField from '@ui/AvatarField';
-
+import colors from '@utils/colors';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons'
 interface Props {
   profile?: UserProfile | null
 }
@@ -11,11 +12,50 @@ const ProfileContainer: FC<Props> = ({profile}) => {
     if(!profile) return null
   return <View style={styles.container}>
     <AvatarField source={profile.avatar}/>
+    <View style={styles.profileInfoContainer}>
+        <Text style={styles.profileName}>{profile.name}</Text>
+        <View style={styles.flexRow}>
+            <Text style={styles.email}>{profile.email}</Text>
+            <MaterialIcon name='verified' size={15} color={colors.SECONDARY}/>
+
+        </View>
+        <View style={styles.flexRow}>
+            <Text style={styles.profileActionLink}>{profile.followers} Followers</Text>
+            <Text style={styles.profileActionLink}>{profile.followings} Following</Text>
+        </View>
+    </View>
   </View>;
 };
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  profileName:{
+    color: colors.CONTRAST,
+    fontSize: 18,
+    fontWeight: '700'
+  },
+  email:{
+    color: colors.CONTRAST,
+    marginRight: 5
+  },
+  flexRow:{
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  profileInfoContainer:{
+    paddingLeft: 10
+  },
+  profileActionLink:{
+    backgroundColor: colors.SECONDARY,
+    color: colors.PRIMARY,
+    paddingHorizontal: 4,
+    paddingVertical: 2,
+    marginRight: 5
+  }
 });
 
 export default ProfileContainer;
