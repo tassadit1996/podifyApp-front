dc86d2d2a8c534368ac1749c604a9033894fafda
tassadit1996 <adoudou.mgh@gmail.com>
2024-02-22 15:39:27 -0300

    logout - 305

--- src/components/profile/ProfileSettings.tsx
+++ src/components/profile/ProfileSettings.tsx
@@ -3,85 +3,114 @@ import AvatarField from '@ui/AvatarField';
 import colors from '@utils/colors';
 import {FC} from 'react';
 import {View, StyleSheet, Text, Pressable, TextInput} from 'react-native';
-import MaterialIcon from 'react-native-vector-icons/MaterialIcons'
-import AntDesign from 'react-native-vector-icons/AntDesign'
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 import AppButton from '@ui/AppButton';
+import {getClient} from 'src/api/client';
+import catchAsyncError from 'src/api/catchError';
+import {useDispatch} from 'react-redux';
+import {updateNotification} from 'src/store/notification';
+import {Keys, removeFromAsyncStorage} from '@utils/asyncStorage';
+import {
+  updateBusyState,
+  updateLoggedInState,
+  updateProfile,
+} from 'src/store/auth';
 
 interface Props {}
 
-const ProfileSettings: FC<Props> = (props) => {
-  return <View style={styles.container}>
-    <AppHeader title="Settings"/>
-    <Text style={{color: 'white', fontSize: 25}}>ProfileSettings</Text>
-    <View style={styles.titleContainer}>
+const ProfileSettings: FC<Props> = props => {
+  const dispatch = useDispatch();
+  const handleLogout = async (fromAll?: boolean) => {
+    dispatch(updateBusyState(true));
+    try {
+      const endpoint = '/auth/log-out?fromAll=' + (fromAll ? 'yes' : '');
+      const client = await getClient();
+      await client.post(endpoint);
+      await removeFromAsyncStorage(Keys.AUTH_TOKEN);
+      dispatch(updateProfile(null));
+      dispatch(updateLoggedInState(false));
+    } catch (error) {
+      const errorMessage = catchAsyncError(error);
+      dispatch(updateNotification({message: errorMessage, type: 'error'}));
+    }
+    dispatch(updateBusyState(false));
+  };
+
+  return (
+    <View style={styles.container}>
+      <AppHeader title="Settings" />
+      <Text style={{color: 'white', fontSize: 25}}>ProfileSettings</Text>
+      <View style={styles.titleContainer}>
         <Text style={styles.title}>Profile Settings</Text>
-    </View>
+      </View>
 
-    <View style={styles.settingOptionsContainer}>
+      <View style={styles.settingOptionsContainer}>
         <View style={styles.avatarContainer}>
-            <AvatarField/>
-            <Pressable style={styles.paddingLeft}>
-                <Text style={styles.linkText}>Update Profile Image</Text>
-            </Pressable>
+          <AvatarField />
+          <Pressable style={styles.paddingLeft}>
+            <Text style={styles.linkText}>Update Profile Image</Text>
+          </Pressable>
         </View>
-        <TextInput style={styles.nameInput} value={'John'}/>
+        <TextInput style={styles.nameInput} value={'John'} />
         <View style={styles.emailContainer}>
-            <Text style={styles.email}>john@email.com</Text>
-            <MaterialIcon name="verified" size={15} color={colors.CONTRAST}/>
+          <Text style={styles.email}>john@email.com</Text>
+          <MaterialIcon name="verified" size={15} color={colors.CONTRAST} />
         </View>
-    </View>
+      </View>
 
-    <View style={styles.titleContainer}>
+      <View style={styles.titleContainer}>
         <Text style={styles.title}>logout</Text>
-    </View>
+      </View>
 
-    <View style={styles.settingOptionsContainer}>
-        <Pressable style={styles.logoutBtn}>
-            <AntDesign name='logout' size={20} color={colors.CONTRAST} />
-            <Text style={styles.logoutBtnTitle}>Logout From All</Text>
+      <View style={styles.settingOptionsContainer}>
+        <Pressable onPress={() => handleLogout(true)} style={styles.logoutBtn}>
+          <AntDesign name="logout" size={20} color={colors.CONTRAST} />
+          <Text style={styles.logoutBtnTitle}>Logout From All</Text>
         </Pressable>
-        <Pressable style={styles.logoutBtn}>
-            <AntDesign name='logout' size={20} color={colors.CONTRAST} />
-            <Text style={styles.logoutBtnTitle}>Logout</Text>
+        <Pressable onPress={() => handleLogout()} style={styles.logoutBtn}>
+          <AntDesign name="logout" size={20} color={colors.CONTRAST} />
+          <Text style={styles.logoutBtnTitle}>Logout</Text>
         </Pressable>
+      </View>
+      <View style={styles.marginTop}>
+        <AppButton title="update" borderRadius={7} />
+      </View>
     </View>
-    <View style={styles.marginTop}>
-        <AppButton title='update' borderRadius={7}/>
-    </View>
-  </View>;
- }
- 
- const styles = StyleSheet.create({
-   container: {
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
     padding: 10,
-   },
-   titleContainer: {
+  },
+  titleContainer: {
     borderBottomWidth: 0.5,
     borderBottomColor: colors.SECONDARY,
     marginTop: 15,
-    paddingTop: 15
-   },
-   title: {
+    paddingTop: 15,
+  },
+  title: {
     fontWeight: 'bold',
     fontSize: 18,
-    color: colors.SECONDARY
-   },
-   settingOptionsContainer:{
+    color: colors.SECONDARY,
+  },
+  settingOptionsContainer: {
     marginTop: 15,
     paddingLeft: 15,
-   },
-   avatarContainer: {
+  },
+  avatarContainer: {
     flexDirection: 'row',
-    alignItems: 'center'
-   },
-   linkText:{
+    alignItems: 'center',
+  },
+  linkText: {
     color: colors.SECONDARY,
     fontStyle: 'italic',
-   },
-   paddingLeft: {
-    paddingLeft: 15
-   },
-   nameInput: {
+  },
+  paddingLeft: {
+    paddingLeft: 15,
+  },
+  nameInput: {
     color: colors.CONTRAST,
     fontWeight: 'bold',
     fontSize: 18,
@@ -90,29 +119,29 @@ const ProfileSettings: FC<Props> = (props) => {
     borderColor: colors.CONTRAST,
     borderRadius: 7,
     marginTop: 15,
-   },
-   emailContainer: {
+  },
+  emailContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    marginTop: 15
-   },
-   email: {
+    marginTop: 15,
+  },
+  email: {
     color: colors.CONTRAST,
     marginLeft: 10,
-   },
-   logoutBtn: {
+  },
+  logoutBtn: {
     flexDirection: 'row',
     alignItems: 'center',
     marginTop: 15,
-   },
-   logoutBtnTitle: {
+  },
+  logoutBtnTitle: {
     color: colors.CONTRAST,
     fontSize: 18,
-    marginLeft: 5
-   },
-   marginTop: {
+    marginLeft: 5,
+  },
+  marginTop: {
     marginTop: 15,
-   }
-}) 
-  
-export default ProfileSettings; 
+  },
+});
+
+export default ProfileSettings;
