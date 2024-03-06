bd2adc8b481536ade4b02bf9aaf8412e3cf583ec
tassadit1996 <adoudou.mgh@gmail.com>
2024-03-06 08:44:28 -0300

    audio progress line

--- src/components/MiniAudioPlayer.tsx
+++ src/components/MiniAudioPlayer.tsx
@@ -7,6 +7,8 @@ import AntDesign from 'react-native-vector-icons/AntDesign';
 import PlayPauseBtn from '@ui/PlayPauseBtn';
 import useAudioController from 'src/hooks/useAudioController';
 import Loader from '@ui/loader';
+import {mapRange} from '@utils/math';
+import {useProgress} from 'react-native-track-player';
 
 interface Props {}
 
@@ -15,28 +17,57 @@ export const MiniPlayerHeight = 60;
 const MiniAudioPlayer: FC<Props> = props => {
   const {onGoingAudio} = useSelector(getPlayerState);
   const {isBusy, isPlaying, togglePlayPause} = useAudioController();
+  const progress = useProgress();
 
   const poster = onGoingAudio?.poster;
   const source = poster ? {uri: poster} : require('../assets/music.png');
 
   return (
-    <View style={styles.container}>
-      <Image source={source} style={styles.poster} />
-      <View style={styles.contentContainer}>
-        <Text style={styles.title}>{onGoingAudio?.title}</Text>
-        <Text style={styles.name}>{onGoingAudio?.owner.name}</Text>
+    <>
+      <View
+        style={{
+          height: 2,
+          backgroundColor: colors.SECONDARY,
+          width: `${mapRange({
+            outputMin: 0,
+            outputMax: 100,
+            inputMin: 0,
+            inputMax: progress.duration,
+            inputValue: progress.position
+          })}%`,
+        }}
+      />
+      <View
+        style={{
+          height: 2,
+          backgroundColor: colors.SECONDARY,
+          width: `${mapRange({
+            outputMin: 0,
+            outputMax: 100,
+            inputMin: 0,
+            inputMax: progress.duration,
+            inputValue: progress.position,
+          })}%`,
+        }}
+      />
+      <View style={styles.container}>
+        <Image source={source} style={styles.poster} />
+        <View style={styles.contentContainer}>
+          <Text style={styles.title}>{onGoingAudio?.title}</Text>
+          <Text style={styles.name}>{onGoingAudio?.owner.name}</Text>
+        </View>
+
+        <Pressable style={{paddingHorizontal: 10}}>
+          <AntDesign name="hearto" size={24} color={colors.CONTRAST} />
+        </Pressable>
+
+        {isBusy ? (
+          <Loader />
+        ) : (
+          <PlayPauseBtn playing={isPlaying} onPress={togglePlayPause} />
+        )}
       </View>
-
-      <Pressable style={{paddingHorizontal: 10}}>
-        <AntDesign name="hearto" size={24} color={colors.CONTRAST} />
-      </Pressable>
-
-      {isBusy ? (
-        <Loader />
-      ) : (
-        <PlayPauseBtn playing={isPlaying} onPress={togglePlayPause} />
-      )}
-    </View>
+    </>
   );
 };
 
