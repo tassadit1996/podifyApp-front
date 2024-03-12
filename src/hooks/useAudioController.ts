3c0746ddafba1d3acef787c7e0e6f1e886c81af5
tassadit1996 <adoudou.mgh@gmail.com>
2024-03-12 08:31:24 -0300

    sending history request

--- src/hooks/useAudioController.ts
+++ src/hooks/useAudioController.ts
@@ -134,7 +134,7 @@ const useAudioController = () => {
 
       await TrackPlayer.setupPlayer();
       await TrackPlayer.updateOptions({
-        progressUpdateEventInterval: 1,
+        progressUpdateEventInterval: 10,
         android: {
           appKilledPlaybackBehavior:
             AppKilledPlaybackBehavior.StopPlaybackAndRemoveNotification,
--- src/playbackService.ts
+++ src/playbackService.ts
@@ -1,34 +1,32 @@
 import TrackPlayer, {Event} from 'react-native-track-player';
+import {getClient} from './api/client';
 
 const playbackService = async () => {
   TrackPlayer.addEventListener(Event.RemotePlay, () => {
-    TrackPlayer.play()
-    
+    TrackPlayer.play();
   });
   TrackPlayer.addEventListener(Event.RemotePause, () => {
-    TrackPlayer.pause()
-    
+    TrackPlayer.pause();
   });
   TrackPlayer.addEventListener(Event.RemoteNext, () => {
-    TrackPlayer.skipToNext()
-    
+    TrackPlayer.skipToNext();
   });
   TrackPlayer.addEventListener(Event.RemotePrevious, () => {
-    TrackPlayer.skipToPrevious()
-    
+    TrackPlayer.skipToPrevious();
   });
-  TrackPlayer.addEventListener(Event.PlaybackProgressUpdated, e  => {
-   console.log(e);
+  TrackPlayer.addEventListener(Event.PlaybackProgressUpdated, async e => {
+    const lists = await TrackPlayer.getQueue();
+    const audio = lists[e.track];
+
+    const client = await getClient();
+    await client.post('/history', {
+      audio: audio.id,
+      progress: e.position,
+      date: new Date(Date.now()),
+    }).catch(err => console.log(err))
+
   });
   
-
-
-
 };
-
-
-
-
-
 
 export default playbackService;
