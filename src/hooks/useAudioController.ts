035e12dd35dcffb5b20dd1a843829c98e4630f1e
tassadit1996 <adoudou.mgh@gmail.com>
2024-03-20 09:22:27 -0300

    fetching is favorite

--- src/hooks/useAudioController.ts
+++ src/hooks/useAudioController.ts
@@ -47,10 +47,11 @@ const useAudioController = () => {
     if (!isPlayerReady) {
       //Playing audio for the first time.
       await updateQueue(data);
+      dispatch(updateOnGoingAudio(item));
       const index = data.findIndex(audio => audio.id === item.id);
       await TrackPlayer.skip(index);
       await TrackPlayer.play();
-      dispatch(updateOnGoingAudio(item));
+      
       return dispatch(updateOnGoingList(data));
     }
 
