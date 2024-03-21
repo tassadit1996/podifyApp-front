43d06681f76ed949cb2cee18e14ef8ef81ffd176
tassadit1996 <adoudou.mgh@gmail.com>
2024-03-21 09:32:55 -0300

    oppening playlist profiles

--- src/components/profile/PlaylistTab.tsx
+++ src/components/profile/PlaylistTab.tsx
@@ -2,21 +2,36 @@ import EmptyRecords from '@ui/EmptyRecords';
 import PlaylistItem from '@ui/PlaylistItem';
 import {FC} from 'react';
 import {View, StyleSheet, Text, ScrollView} from 'react-native';
+import {useDispatch} from 'react-redux';
+import {Playlist} from 'src/@types/audio';
 import {useFetchPlaylist} from 'src/hooks/query';
+import {
+  updatePlaylistVisibility,
+  updateSelectedListId,
+} from 'src/store/PlaylistModal';
 
 interface Props {}
 
 const PlaylistTab: FC<Props> = props => {
   const {data, isLoading} = useFetchPlaylist();
+  const dispatch = useDispatch();
 
+  const handleOnListPress = (playlist: Playlist) => {
+    dispatch(updateSelectedListId(playlist.id));
+    dispatch(updatePlaylistVisibility(true));
+  };
   return (
     <ScrollView style={styles.container}>
-      {!data?.length ? (
-        <EmptyRecords title="There is no playlist!"/>
-      ) : null}
-      
+      {!data?.length ? <EmptyRecords title="There is no playlist!" /> : null}
+
       {data?.map(playlist => {
-        return <PlaylistItem key={playlist.id} playlist={playlist} />;
+        return (
+          <PlaylistItem
+            onPress={() => handleOnListPress(playlist)}
+            key={playlist.id}
+            playlist={playlist}
+          />
+        );
       })}
     </ScrollView>
   );
--- src/components/profile/PublicPlaylistTab.tsx
+++ src/components/profile/PublicPlaylistTab.tsx
@@ -2,8 +2,11 @@ import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import PlaylistItem from '@ui/PlaylistItem';
 import {FC} from 'react';
 import {View, StyleSheet, ScrollView} from 'react-native';
+import { useDispatch } from 'react-redux';
+import { Playlist } from 'src/@types/audio';
 import {PublicProfileTabParamsList} from 'src/@types/navigation';
 import {useFetchPublicPlaylist} from 'src/hooks/query';
+import { updatePlaylistVisibility, updateSelectedListId } from 'src/store/PlaylistModal';
 
 type Props = NativeStackScreenProps<
   PublicProfileTabParamsList,
@@ -13,10 +16,23 @@ type Props = NativeStackScreenProps<
 const PublicPlaylistTab: FC<Props> = props => {
   const {data} = useFetchPublicPlaylist(props.route.params.profileId);
 
+  const dispatch = useDispatch();
+
+  const handleOnListPress = (playlist: Playlist) => {
+    dispatch(updateSelectedListId(playlist.id));
+    dispatch(updatePlaylistVisibility(true));
+  };
+
   return (
     <ScrollView style={styles.container}>
       {data?.map(playlist => {
-        return <PlaylistItem key={playlist.id} playlist={playlist} />;
+        return (
+          <PlaylistItem
+            onPress={() => handleOnListPress(playlist)}
+            key={playlist.id}
+            playlist={playlist}
+          />
+        );
       })}
     </ScrollView>
   );
